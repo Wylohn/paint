980b59d21b5e85521a81e073b1aa4ef53457aec9
Wylohn <wylohn.tribout@gmail.com>
2022-01-17 19:08:07 +0100

    add animation on burger menu

--- src/script.js
+++ src/script.js
@@ -1,11 +1,21 @@
-var menuWrapper = document.getElementById('menu');
+var containBurger = document.querySelector('.menu-btn');
 var sideBar = document.getElementById('sidebar');
 var body = document.getElementsByTagName("body")[0];
+let menuOpen = false;
 
-menuWrapper.addEventListener('click', showSidebar);
+
+containBurger.addEventListener('click', showSidebar);
 
 function showSidebar() {
     sideBar.classList.toggle('sidebar-style');
     sideBar.classList.toggle('sidebar-show');
-    body.classList.toggle('body-overflow')
+    body.classList.toggle('body-overflow');
+
+    if (!menuOpen) {
+        containBurger.classList.add('open');
+        menuOpen = true;        
+    } else {
+        containBurger.classList.remove('open');
+        menuOpen = false;
+    }
 }
